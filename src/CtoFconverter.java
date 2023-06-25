7157cab911785760549c78d9314a53a8f97d08b4
NicholasTurner03 <nturner00023@gmail.com>
2023-06-25 15:07:55 -0400

    Final Code

---
+++ src/CtoFconverter.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CtoFconverter {
+
+    public static <Int> void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double F;
+        double C;
+        double temp;
+        boolean done= false;
+        String trash;
+        System.out.println("Please enter the temperature in celsius:");
+        do
+        {
+            if (in.hasNextDouble()) {
+                C = in.nextDouble();
+                done = true;
+            }else{
+                trash=in.nextLine();
+                System.out.println("You said your temperature was: " +trash);
+                System.out.println("Invalid entry, try again");}
+        }while(!done);
+        C=in.nextDouble();
+        F = (C*9/5)+32;
+        System.out.println("The temperature in fahrenheit is "+F);
+        if (F<=32){
+            System.out.println("This is Below Freezing");
+        }
+        if (F>= 212){
+            System.out.println("The temperature is boiling");
+        }
+
+
+    }
+}
